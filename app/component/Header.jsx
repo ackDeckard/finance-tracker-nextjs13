4b759df3858b9539997208e430dcd6255a96441e
Pablo <cadastros@protonmail.com>
2023-02-20 19:03:02 -0300

    first part, header added to the project

---
+++ app/component/Header.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import React from "react";
+
+import { MdQueryStats } from "react-icons/md";
+import Button from "./Button";
+
+function Header() {
+  return (
+    <header className="container mx-auto max-w-3xl px-6 py-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          {/* user image */}
+          <Image
+            src="/steam.jpg"
+            alt="user profile image"
+            width={0}
+            height={0}
+            unoptimized
+            className="mr-2 h-11 w-11 rounded-full"
+          />
+
+          {/* user profile name */}
+          <small>Olá, usuário</small>
+        </div>
+
+        <nav className="flex gap-2">
+          <div>
+            <MdQueryStats size={30} />
+          </div>
+          <div>
+            <Button text={"Sign out"} />
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
